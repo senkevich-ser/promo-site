55d3c79c90558d44c776f5a8eaa4f1a68d45a729
senkevich-ser <72697195+senkevich-ser@users.noreply.github.com>
2022-12-13 07:07:12 +0400

    added cover with ellipses

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import './App.css'
+import Cover from '../Cover/Cover';
 
 
 function App() {
 
   return (
-    <></>
+    <div className='app'><Cover/></div>
   )
 }
 
---
+++ src/components/Cover/Cover.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import './Cover.css';
+
+
+function Cover() {
+
+  return (
+    <>
+    <div className='background'>
+      <div className='ellipse1'></div>
+      <div className='ellipse1 ellipse2'></div>
+      <div className='ellipse1 ellipse3'></div>
+    </div>
+    </>
+  )
+}
+
+export default Cover;
