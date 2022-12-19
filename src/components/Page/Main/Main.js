d772e0a89f189c61710e4df0c76ddf59eb0c118b
Sergey Senkevich <accc75-mai.ru@yandex.ru>
2022-12-19 06:51:48 +0400

    added main section

---
+++ src/components/Page/Main/Main.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import './Main.css';
+
+
+function Main() {
+
+  return (
+    <section className='main'>
+    <dev className='main__info'>
+      <div className='main__titles'>
+        <h1 className='main__titles-bold'>Super NFT</h1>
+        <h2 className='main__titles-normal'> Marketplace</h2>
+      </div>
+      <div className='main__discharge'></div>
+    </dev>
+    <dev className='main__cards'></dev>
+    <p className='main__description'></p>
+    <div className='main__connectButtons'></div>
+    <div className='main__buttonsTitles'></div>
+    <div className='main__connectButtons'></div>
+    <dev className='card'></dev>
+    </section>
+  )
+}
+
+export default Main;
--- src/components/Page/Page.js
+++ src/components/Page/Page.js
@@ -1,6 +1,7 @@
 
 import React from 'react';
 import Header from '../Header/Header';
+import Main from './Main/Main';
 import './Page.css';
 
 
@@ -9,6 +10,7 @@ function Page() {
   return (
     <div className='page'>
     <Header/>
+    <Main/>
     </div>
   )
 }
