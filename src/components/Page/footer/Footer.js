ab7520080666240f18b7abaa3d5e70bba595a693
senkevich-ser <72697195+senkevich-ser@users.noreply.github.com>
2022-12-26 20:29:49 +0400

    added Links in footer

--- src/components/Page/footer/Footer.js
+++ src/components/Page/footer/Footer.js
@@ -1,6 +1,6 @@
 /* eslint-disable jsx-a11y/anchor-has-content */
 import React from "react";
-import { NavLink} from "react-router-dom";
+import { Link} from "react-router-dom";
 import Logo from "../../Logo/Logo";
 import "./Footer.css";
 
@@ -9,39 +9,40 @@ function Footer() {
     <footer className="footer">
       <div className="footer__ColumnWrapper">
       <Logo />
-        <p className="footer__copyrightText">Tempor mollit nisi sunt ad incididunt labore.
+        <p className="footer__text footer__copyrightText">Tempor mollit nisi sunt ad incididunt labore.
           Tempor mollit nisi sunt ad incididunt labore.Tempor mollit nisi sunt ad incididunt labore.</p>
           <ul className="socialLinks footer__subtitles">
             <li><a  href='www.facebook.com' target='_blank' className="socialLink  facebook"></a></li>
             <li><a  href='www.linkedin.com' target='_blank' className="socialLink  likedIn"></a></li>
             <li><a  href='www.twitter.com' target='_blank' className="socialLink  twitter"></a></li>
           </ul>
+          <span className="footer__copyright">Copyrigh &#169; &thinsp; NFT core 2022 All right reserved</span>
       </div>
       <div className="footer__ColumnWrapper">
       <h3 className="footer__tiles">SiteMap</h3>
       <ul className="footer__subtitles">
-        <li className="footer__subtitles"><NavLink to='/'>Home</NavLink></li>
-        <li className="footer__subtitles">About</li>
-        <li className="footer__subtitles">NFT</li>
-        <li className="footer__subtitles">RoadMap</li>
-        <li className="footer__subtitles">Blog</li>
-        <li className="footer__subtitles">Contact</li>
+        <li className="footer__subtitles"><Link className='footer__navLink footer__text' to='/'>Home</Link></li>
+        <li className="footer__subtitles"><Link className='footer__navLink footer__text' to='/'>About</Link></li>
+        <li className="footer__subtitles"><Link className='footer__navLink footer__text' to='/'>NFT</Link></li>
+        <li className="footer__subtitles"><Link className='footer__navLink footer__text' to='/'>RoadMap</Link></li>
+        <li className="footer__subtitles"><Link className='footer__navLink footer__text' to='/'>Blog</Link></li>
+        <li className="footer__subtitles"><Link className='footer__navLink footer__text' to='/'>Contact</Link></li>
       </ul>
       </div>
       <div className="footer__ColumnWrapper">
       <h3 className="footer__tiles" >Company</h3>
       <ul className="footer__subtitles">
-        <li className="footer__subtitles">Help&Support</li>
-        <li className="footer__subtitles">Terms&Condition</li>
-        <li className="footer__subtitles">Privacy Policy</li>
+        <li className="footer__subtitles"><Link className='footer__navLink footer__txt' to='/'>Help&Support</Link></li>
+        <li className="footer__subtitles"><Link className='footer__navLink footer__text' to='/'>Terms&Condition</Link></li>
+        <li className="footer__subtitles"><Link className='footer__navLink footer__text' to='/'>Privacy Policy</Link></li>
       </ul>
       </div>
       <div className="footer__ColumnWrapper">
       <h3 className="footer__tiles">Resource</h3>
       <ul className="footer__subtitles">
-        <li className="footer__subtitles">Partner</li>
-        <li className="footer__subtitles">Blog</li>
-        <li className="footer__subtitles">Newsletter</li>
+        <li className="footer__subtitles"><Link className='footer__navLink footer__text' to='/'>Partner</Link></li>
+        <li className="footer__subtitles"><Link className='footer__navLink footer__text' to='/'>Blog</Link></li>
+        <li className="footer__subtitles"><Link className='footer__navLink footer__text' to='/'>Newsletter</Link></li>
       </ul>
       </div>
     </footer>
