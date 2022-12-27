0fd05cec077224987fce5e8f7e5c8b1b35431fd5
Sergey Senkevich <accc75-mail.ru@yandex.ru>
2022-12-27 17:42:55 +0400

    added Spiner in App

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,13 +1,29 @@
 import React, { useEffect, useState }from 'react';
+import Spinner from '../Spinner/Spinner';
 import './App.css'
 import Cover from '../Cover/Cover';
 import Page from '../Page/Page';
 
 
 function App() {
-  return (
+  const [isLoading, setIsLoading] = useState(false);
+  const [allCards, setAllCards] = useState([]);
+
+  useEffect(() => {
+    setIsLoading(true);
+    fetch('https://dummyjson.com/products')
+.then(res => res.json())
+.then((res)=>{
+  setAllCards(res.products);
+  setIsLoading(false);
+});
+  }, []);
+
+  return isLoading ? (
+    <Spinner />
+  ) : (
     <div className='app'>
-        <Cover children={<Page />} />
+        <Cover children={<Page allCards={allCards} />} />
     </div>
   )
 }
--- src/components/Page/Page.js
+++ src/components/Page/Page.js
@@ -5,34 +5,17 @@ import Main from './Main/Main';
 import Choose from './Choose/Chose';
 import Auction from './Auction/Auction';
 import Footer from './footer/Footer';
-import Spinner from '../Spinner/Spinner';
 import './Page.css';
 
 
-function Page() {
+function Page(props) {
 
-  const [allCards, setAllCards] = useState([]);
-  const [isLoading, setIsLoading] = useState(false);
-
-  useEffect(() => {
-    setIsLoading(true);
-    fetch('https://dummyjson.com/products')
-.then(res => res.json())
-.then((res)=>{
-  setAllCards(res.products);
-  setIsLoading(false);
-});
-  }, []);
-
-
-  return isLoading ? (
-    <Spinner />
-  ) : (
+  return (
     <div className='page'>
     <Header/>
-    <Main cards={allCards.slice(0,2)}/>
+    <Main cards={props.allCards.slice(0,2)}/>
     <Choose/>
-    <Auction cards={allCards.slice(4,16)}/>
+    <Auction cards={props.allCards.slice(4,16)}/>
     <Footer/>
     </div>
   )
