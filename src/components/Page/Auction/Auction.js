d2c876b1aecb3a17bc0c2a2fa782139dfb151dad
Sergey Senkevich <accc75-mail.ru@yandex.ru>
2023-01-08 11:13:53 +0400

    fixed links in footer

--- src/components/Page/Auction/Auction.js
+++ src/components/Page/Auction/Auction.js
@@ -1,35 +1,30 @@
-import React,{useEffect} from 'react';
+import React, { useEffect } from 'react';
 import Card from '../../Card/Card';
 import CardAuction from '../../CardAuction/CardAuction';
 import { headerRoundButtonStyles } from '../Main/styles';
-import { Button , Space } from 'antd';
+import { Button, Space } from 'antd';
 import './Auction.css'
 
 
 
 function Auction(props) {
-useEffect(()=>{
-props.allCards.map((card)=>(
-  console.log(card.category)
-))
-},[])
   return (
     <section className='auction'>
       <h2 className='auction__title'>{props.title}</h2>
       <h3 className='auction__subtitle'>{props.subtitle}</h3>
       <div className='auction__card-container'>
-        {props.allCards.map((card,i) => {
+        {props.allCards.map((card) => {
           return (
-            <Card  onClick={props.onClick}
-            isDeleteButton={props.isDeleteButton}
-            children={<CardAuction isRoundCheckBox={props.isRoundCheckBox}
-            dataCard={card}/>}
-            dataCard={card} style='card_type_auction' key={i}/>
+            <Card onClick={props.onClick}
+              isDeleteButton={props.isDeleteButton}
+              children={<CardAuction isRoundCheckBox={props.isRoundCheckBox}
+                dataCard={card} />}
+              dataCard={card} style={'card_type_auction'} key={card.id} />
           );
         })}
       </div>
       <Space wrap>
-      <Button type="primary" style={headerRoundButtonStyles} shape="round" >Explore more</Button>
+        <Button type="primary" style={headerRoundButtonStyles} shape="round" >Explore more</Button>
       </Space>
     </section>
   )
